b2a1cf79bd03f0449b0ae8e150cb53933c4e124a
SH4ND <miqueiasantos20@gmail.com>
2023-11-05 17:41:24 -0300

    alioth: Remove Dolby Atmos
    This reverts the commits:
    
    1 - Revert "alioth: parts: Introduce pt-rBR translation"
    2 - Revert "alioth: Import Dolby audio blobs"
    3 - Revert "alioth: parts: Always refresh playback if status changed"
    4 - Revert "alioth: parts: Introduce Dolby Atmos"
    
    Change-Id: I074962c2398271475ab812e7769f647ac38e179f

--- device.mk
+++ device.mk
@@ -74,9 +74,6 @@ PRODUCT_ODM_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
-PRODUCT_PACKAGES += \
-    DolbyManager
-
 # Bluetooth
 PRODUCT_VENDOR_PROPERTIES += \
     persist.sys.fflag.override.settings_bluetooth_hearing_aid=true \
@@ -232,15 +229,6 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.crypto.allow_encrypt_override=true \
     ro.crypto.volume.filenames_mode="aes-256-cts" \
     ro.hardware.keystore_desede=true
-
-# Media
-PRODUCT_PACKAGES += \
-    libavservices_minijail_vendor \
-    libcodec2_soft_common.vendor \
-    libsfplugin_ccodec_utils.vendor
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
 
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += \
